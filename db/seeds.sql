9ab777ed50bce5758bb351406d865b317889c2bd
Steven Bender <sbender1990@gmail.com>
2018-04-25 23:03:37 -0500

    working on server

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Burger 1 TEST", false);
+INSERT INTO burgers (burger_name) VALUES ("Burger 2 TEST TEST", false);
+INSERT INTO burgers (burger_name) VALUEs ("Burger 3 TEST TEST TEST", false);
